a5947f8a3e318c18c4f793d165012a8861d824ec
Ionut Mihaescu <ionut.mihaescu@gmail.com>
2019-05-28 11:30:09 +0200

    more conditionals

--- src/hello/conditionals.go
+++ src/hello/conditionals.go
@@ -5,3 +5,42 @@ import "fmt"
 func startConditionals() {
 	fmt.Println("****Starting Conditionals****")
 }
+
+func forLoop() (sum int) {
+
+	sum = 0
+
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	return
+}
+
+func forContinued() int {
+	sum := 1
+
+	for sum < 1000 {
+		sum += sum
+		fmt.Println(sum)
+	}
+	return sum
+}
+
+func ifStatement(x, y int) bool {
+
+	if x > y {
+		return true
+	}
+
+	return false
+}
+
+func ifStatement2() int {
+	var test = true
+
+	if test {
+		return 1
+	} else {
+		return 0
+	}
+}
--- src/hello/hello.go
+++ src/hello/hello.go
@@ -11,5 +11,9 @@ func main() {
 	fmt.Printf("The time is %s \n", time.Now())
 	fmt.Println("My favorite number is:", rand.Intn(10))
 	startConditionals()
+	fmt.Println(forLoop())
+	fmt.Println(forContinued())
+	fmt.Println(ifStatement(8, 6))
+	fmt.Println(ifStatement2())
 
 }
